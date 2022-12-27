04f3b1b636513ce6a60e1ed7365e5280b320b93b
Yuki.kaco <a0936660023@gmail.com>
2022-12-27 15:33:14 +0800

    Update

---
+++ Assets/Scripts/ObstacleFly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
